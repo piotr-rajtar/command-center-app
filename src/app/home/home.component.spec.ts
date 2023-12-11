5a5cd0fedf353bf442f9eadb5fe1fab0cb97782d
Piotr Rajtar <piotrek.rajtar@gmail.com>
2023-12-11 12:43:36 +0100

    add tests to home component

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,23 +1,54 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed, fakeAsync, flush } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HomeComponent } from './home.component';
+
+@Component({})
+class DummyComponent {};
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [
+        HomeComponent,
+        RouterTestingModule.withRoutes([
+          { path: 'inbox', component: DummyComponent },
+          { path: 'lists', component: DummyComponent }
+        ]),
+      ],
     })
     .compileComponents();
-    
+
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should navigate to inbox route after open button click', fakeAsync(() => {
+    component.openCard('inbox');
+
+    flush();
+
+    expect(router.url).toBe('/inbox');
+  }));
+
+  it('should navigate to inbox route after open button click', fakeAsync(() => {
+    component.openCard('lists');
+
+    flush();
+
+    expect(router.url).toBe('/lists');
+  }));
 });
