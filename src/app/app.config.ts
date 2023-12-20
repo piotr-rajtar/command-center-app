55b2e6d10ccd6b53fe64b71728fa01dddd2e239d
Piotr Rajtar <piotrek.rajtar@gmail.com>
2023-12-20 16:38:16 +0100

    prepare api service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import {
   PreloadAllModules,
   provideRouter,
@@ -9,13 +10,16 @@ import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
 import { INBOX_FEATURE_KEY, inboxReducer } from './inbox/store/inbox.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideStore({
-      [INBOX_FEATURE_KEY]: inboxReducer
+        [INBOX_FEATURE_KEY]: inboxReducer
     }),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
-  ],
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects()
+],
 };
---
+++ src/app/inbox/services/ideas-http.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+import { Idea } from '../typings';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IdeasHttpService {
+  private httpClient = inject(HttpClient);
+
+  createIdea(idea: string): Observable<Idea> {
+    return this.httpClient
+      .post<{ name: string }>(
+        'https://angular-command-center-default-rtdb.europe-west1.firebasedatabase.app/ideas.json',
+        JSON.stringify(idea),
+      )
+      .pipe(
+        map(response => ({ id: response.name, content: idea }))
+      );
+  }
+
+  deleteIdea(ideaId: string): Observable<{ ideaId: string }> {
+    return this.httpClient.delete<{ ideaId: string }>(
+      `https://angular-command-center-default-rtdb.europe-west1.firebasedatabase.app/ideas/${ideaId}`,
+    )
+  }
+
+  readIdeas(): Observable<Idea[]> {
+    return this.httpClient.get<Record<string, string>>(
+      'https://angular-command-center-default-rtdb.europe-west1.firebasedatabase.app/ideas.json'
+    ).pipe(
+      map(response => Object.entries(response).map(([id, content]) => ({ id, content }))),
+    );
+  }
+
+  updateIdea(idea: Idea): Observable<string> {
+    return this.httpClient.put<string>(
+      `https://angular-command-center-default-rtdb.europe-west1.firebasedatabase.app/ideas/${idea.id}.json`,
+      JSON.stringify(idea.content),
+    );
+  }
+}
