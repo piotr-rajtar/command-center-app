8d77d5201d50d1d12a1ab0fdc6a840c615e673d9
Piotr Rajtar <piotrek.rajtar@gmail.com>
2023-12-22 15:48:26 +0100

    connect store to inbox-container

--- src/app/inbox/components/inbox-container/inbox-container.component.spec.ts
+++ src/app/inbox/components/inbox-container/inbox-container.component.spec.ts
@@ -1,17 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { InboxContainerComponent } from './inbox-container.component';
 
 describe('InboxContainerComponent', () => {
   let component: InboxContainerComponent;
   let fixture: ComponentFixture<InboxContainerComponent>;
+  let store: MockStore;
+
+  const initialState = {
+    inbox: {
+      isEditMode: false,
+      ideas: [],
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InboxContainerComponent]
+      imports: [InboxContainerComponent],
+      providers: [provideMockStore({ initialState })],
     })
     .compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(InboxContainerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +31,47 @@ describe('InboxContainerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display EditIdeaForm component when edit mode is on', () => {
+    store.setState({
+      inbox: {
+        isEditMode: true,
+        ideas: [],
+      },
+    });
+
+    fixture.detectChanges();
+
+    const editIdeaFormDe = fixture.debugElement.query(By.css('app-edit-idea-form'));
+
+    expect(editIdeaFormDe).toBeTruthy();
+  });
+
+  it('should display AddIdeaForm component when edit mode is off', () => {
+    const addIdeaFormDe = fixture.debugElement.query(By.css('app-add-idea-form'));
+
+    expect(addIdeaFormDe).toBeTruthy();
+  });
+
+  it('should display IdaList component when some ideas present', () => {
+    store.setState({
+      inbox: {
+        isEditMode: false,
+        ideas: [{ id: '1', content: 'Test' }],
+      },
+    });
+
+    fixture.detectChanges();
+
+    const ideaListDe = fixture.debugElement.query(By.css('app-idea-list'));
+
+    expect(ideaListDe).toBeTruthy();
+  });
+
+  it('should not display IdeaList component when no ideas added', () => {
+    const ideaListDe = fixture.debugElement.query(By.css('app-idea-list'));
+
+    expect(ideaListDe).toBeFalsy();
   });
 });
--- src/app/inbox/components/inbox-container/inbox-container.component.ts
+++ src/app/inbox/components/inbox-container/inbox-container.component.ts
@@ -1,42 +1,46 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
+import { AppState } from '../../../typings/store';
+
+import * as InboxActions from '../../store/inbox.actions';
+import * as InboxSelectors from '../../store/inbox.selectors';
 import { Idea } from '../../typings';
 
 import { AddIdeaFormComponent } from '../add-idea-form/add-idea-form.component';
 import { EditIdeaFormComponent } from '../edit-idea-form/edit-idea-form.component';
-
 import { IdeaListComponent } from '../idea-list/idea-list.component';
 
 @Component({
   selector: 'app-inbox',
   standalone: true,
-  imports: [FormsModule, IdeaListComponent, AddIdeaFormComponent, EditIdeaFormComponent],
+  imports: [
+    CommonModule,
+    AddIdeaFormComponent,
+    EditIdeaFormComponent,
+    IdeaListComponent,
+  ],
   templateUrl: './inbox-container.component.html',
   styleUrl: './inbox-container.component.scss'
 })
-export class InboxContainerComponent {
-  idea = '';
-  ideas: Idea[] = [];
+export class InboxContainerComponent implements OnDestroy, OnInit {
+  private store = inject<Store<AppState>>(Store);
 
-  isIdeaFormInEditMode = false;
+  ideas$!: Observable<Idea[]>
+  isEditMode$!: Observable<boolean>;
 
-  addIdea(idea: string) {
+  ngOnInit(): void {
+    this.ideas$ = this.store.select(InboxSelectors.selectIdeas);
+    this.isEditMode$ = this.store.select(InboxSelectors.selectIsEditMode);
   }
 
-  editIdea(idea: string) {
-    //TODO: handle idea edition
+  ngOnDestroy(): void {
+    this.store.dispatch(InboxActions.stopEditMode());
   }
 
   assignIdea(ideaId: string) {
     //TODO: handle idea assignment
-  }
-
-  setEditMode(ideaId: string) {
-    //TODO: handle idea edition
-  }
-
-  removeIdea(ideaId: string) {
-    //TODO: handle idea removal
   }
 }
